53cefb58d6531097d21a3ba989354b845d3ea0ae
DESKTOP-8AQSHC6\nb laptop <abdul.rafay@thinkdigitally.com>
2026-08-18 06:33:15 +0500

    Final touch

--- dist/src/modules/calls/calls.controller.js
+++ dist/src/modules/calls/calls.controller.js
@@ -15,12 +15,53 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.CallsController = void 0;
 const common_1 = require("@nestjs/common");
 const current_user_decorator_1 = require("../../common/decorators/current-user.decorator");
+const config_1 = require("@nestjs/config");
 const entities_1 = require("../../entities");
 const calls_service_1 = require("./calls.service");
 let CallsController = class CallsController {
     svc;
-    constructor(svc) {
+    config;
+    constructor(svc, config) {
         this.svc = svc;
+        this.config = config;
+    }
+    async getTurnCredentials() {
+        const apiKey = this.config.get('METERED_API_KEY');
+        const appName = this.config.get('METERED_APP_NAME');
+        if (apiKey && appName) {
+            try {
+                const res = await fetch(`https://${appName}.metered.live/api/v1/turn/credentials?apiKey=${apiKey}`);
+                if (res.ok) {
+                    const iceServers = await res.json();
+                    return { iceServers };
+                }
+            }
+            catch (e) {
+            }
+        }
+        return {
+            iceServers: [
+                { urls: 'stun:stun.l.google.com:19302' },
+                { urls: 'stun:stun1.l.google.com:19302' },
+                { urls: 'stun:stun2.l.google.com:19302' },
+                { urls: 'stun:global.stun.twilio.com:3478' },
+                {
+                    urls: 'turn:openrelay.metered.ca:80',
+                    username: 'openrelayproject',
+                    credential: 'openrelayproject',
+                },
+                {
+                    urls: 'turn:openrelay.metered.ca:443',
+                    username: 'openrelayproject',
+                    credential: 'openrelayproject',
+                },
+                {
+                    urls: 'turn:openrelay.metered.ca:443?transport=tcp',
+                    username: 'openrelayproject',
+                    credential: 'openrelayproject',
+                },
+            ],
+        };
     }
     history(user) {
         return this.svc.history(user.id);
@@ -33,6 +74,12 @@ let CallsController = class CallsController {
     }
 };
 exports.CallsController = CallsController;
+__decorate([
+    (0, common_1.Get)('turn-credentials'),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", []),
+    __metadata("design:returntype", Promise)
+], CallsController.prototype, "getTurnCredentials", null);
 __decorate([
     (0, common_1.Get)(),
     __param(0, (0, current_user_decorator_1.CurrentUser)()),
@@ -59,6 +106,7 @@ __decorate([
 ], CallsController.prototype, "updateStatus", null);
 exports.CallsController = CallsController = __decorate([
     (0, common_1.Controller)('calls'),
-    __metadata("design:paramtypes", [calls_service_1.CallsService])
+    __metadata("design:paramtypes", [calls_service_1.CallsService,
+        config_1.ConfigService])
 ], CallsController);
 //# sourceMappingURL=calls.controller.js.map
